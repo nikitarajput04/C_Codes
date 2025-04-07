76b0545f603c8ba61d35c33037d6403a042e7a7f
Nikita Rajput <nikitarajput0701@gmail.com>
2025-04-07 18:04:38 +0530

    Question5

---
+++ Codes/mul_num.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int num1,num2;
+    printf(" enter two number:");
+    scanf("%d %d",&num1,&num2);
+    printf("the multiplication of two number is :%d",num1*num2);
+    return 0;
+}
