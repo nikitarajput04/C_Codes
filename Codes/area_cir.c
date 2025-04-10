c7ab9f775a5baaacb7071875671c16401a3b3b95
Nikita Rajput <nikitarajput0701@gmail.com>
2025-04-10 23:26:02 +0530

    Question14

---
+++ Codes/area_cir.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int rad;
+    printf("Enter the value of radius is :");
+    scanf("%d",&rad);
+     float area =3.14*rad*rad;
+    printf("the area of circle is: %f ",area);
+}
