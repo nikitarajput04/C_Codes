16399d7f39d0df8efb4bf31756d4b1538db6fe2b
Nikita Rajput <nikitarajput0701@gmail.com>
2025-04-10 19:31:47 +0530

    Question13

---
+++ Codes/squa_cube.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int x;
+printf("enter the value of x:");
+scanf("%d",&x);
+int square=x*x;
+printf(" the square of%d is :%d\n",x,square);
+int cube=x*x*x;
+printf(" the cube of%d is :%d",x,cube);
+}
