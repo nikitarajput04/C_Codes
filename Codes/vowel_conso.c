ff166d5f81793585e9b9668b1f0ba204fb2dc840
Nikita Rajput <nikitarajput0701@gmail.com>
2025-04-12 23:11:40 +0530

    Question18

---
+++ Codes/vowel_conso.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    char ch;
+    printf("Enter alphabet :");
+    scanf("%c",&ch);
+    if( ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+        printf("%c is vowel\n",ch);
+
+    }
+    else{
+        printf("%c is consonant \n",ch);
+
+    }
+}
