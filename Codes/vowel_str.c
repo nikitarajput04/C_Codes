e16411b138fe8c69113fe7f31e7f8fa8dded4655
Nikita Rajput <nikitarajput0701@gmail.com>
2025-04-17 23:54:50 +0530

    Question34

---
+++ Codes/vowel_str.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    char a[100];
+    int i = 0, vowel = 1;
+
+    printf("Enter the string: ");
+    fgets(a, sizeof(a), stdin);
+
+    while (a[i] != '\0') {
+        char ch = a[i];
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+            ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
+            vowel++;
+        }
+        i++;
+    }
+
+    printf("\nTotal vowel = %d\n", vowel);
+    return 0;
+}
