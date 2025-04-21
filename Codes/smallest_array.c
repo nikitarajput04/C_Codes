8e37def1d312741ac25ed6581e9cf3f2112c7f01
Nikita Rajput <nikitarajput0701@gmail.com>
2025-04-21 12:39:27 +0530

    Question41

---
+++ Codes/smallest_array.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int n;
+     int arr[100];
+    printf("enter number of element of array:");
+    scanf("%d",&n);
+    for(int i=0;i<=n;i++){
+printf("entr the num %d :",i+1);
+    scanf("%d",&arr[i]);
+ } 
+ for(int i=1;i<=n;i++){
+    if(arr[0]>arr[i]){
+        arr[0]=arr[i];
+    }
+ }
+  printf("the largest element is=%d",arr[0]);  
+}
