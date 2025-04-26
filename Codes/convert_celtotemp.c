a2c956cb910ae2a4b2da46f136651defc38de915
Nikita Rajput <nikitarajput0701@gmail.com>
2025-04-26 22:42:25 +0530

    Question51

---
+++ Codes/convert_celtotemp.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int choice;
+    float temp, converted;
+
+    printf("Temperature Converter\n");
+    printf("1. Celsius to Fahrenheit\n");
+    printf("2. Fahrenheit to Celsius\n");
+    printf("Enter your choice (1 or 2): ");
+    scanf("%d", &choice);
+
+    if (choice == 1) {
+        printf("Enter temperature in Celsius: ");
+        scanf("%f", &temp);
+        converted = (temp * 9 / 5) + 32;
+        printf("%.2f Celsius = %.2f Fahrenheit\n", temp, converted);
+    } else if (choice == 2) {
+        printf("Enter temperature in Fahrenheit: ");
+        scanf("%f", &temp);
+        converted = (temp - 32) * 5 / 9;
+        printf("%.2f Fahrenheit = %.2f Celsius\n", temp, converted);
+    } else {
+        printf("Invalid choice!\n");
+    }
+
+    return 0;
+}
