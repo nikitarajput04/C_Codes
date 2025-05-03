1b8ef7b7723a950c85b15df901fde2013b59c953
Nikita Rajput <nikitarajput0701@gmail.com>
2025-05-03 22:38:10 +0530

    Question63

--- Codes/foor.c
+++ Codes/foor.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 int main(){
-int n;
-printf("Enter a number:");
-scanf("%d",&n);
+//int n;
+//printf("Enter a number:");
+//scanf("%d",&n);
 int a=100;
-for(int i=1;i<=n;i++){
+for(int i=1;a>0;i++){
     
     printf("%d ",a);
     a=a-3;
