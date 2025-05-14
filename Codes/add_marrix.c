3b12cef3e391ea87ec1a38b55f83647ce3195808
Nikita Rajput <nikitarajput0701@gmail.com>
2025-05-14 22:53:39 +0530

    Question81

---
+++ Codes/add_marrix.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+#define ROW 3
+#define COL 3
+
+int main() {
+    int matrix1[ROW][COL], matrix2[ROW][COL], sum[ROW][COL];
+
+   
+    printf("Enter elements of first %dx%d matrix:\n", ROW, COL);
+    for (int i = 0; i < ROW; i++) {
+        for (int j = 0; j < COL; j++) {
+            printf("matrix1[%d][%d] = ", i, j);
+            scanf("%d", &matrix1[i][j]);
+        }
+    }
+    printf("Enter elements of second %dx%d matrix:\n", ROW, COL);
+    for (int i = 0; i < ROW; i++) {
+        for (int j = 0; j < COL; j++) {
+            printf("matrix2[%d][%d] = ", i, j);
+            scanf("%d", &matrix2[i][j]);
+        }
+    }
+    for (int i = 0; i < ROW; i++) {
+        for (int j = 0; j < COL; j++) {
+            sum[i][j] = matrix1[i][j] + matrix2[i][j];
+        }
+    }
+
+    printf("\nSum of the matrices:\n");
+    for (int i = 0; i < ROW; i++) {
+        for (int j = 0; j < COL; j++) {
+            printf("%d\t", sum[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
